c785fb69e8d61ba67bc0a1cde4a99ebaba8f2853
gzhovnu <g.zhovnu@gmail.com>
2015-10-16 13:03:58 +0300

    polygons

--- books/forms.py
+++ books/forms.py
@@ -2,13 +2,19 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    email = forms.EmailField(required=False, label='Your e-mail address')
-    message = forms.CharField(widget=forms.Textarea)
+    lat_from = forms.CharField(max_length=9)
+    lon_from = forms.CharField(max_length=9)
+    lat_to = forms.CharField(max_length=9)
+    lon_to = forms.CharField(max_length=9)
+    people = forms.IntegerField()
+    data = forms.DateTimeField()
+    time = forms.TimeField()
+
+
 
     def clean_message(self):
-        message = self.cleaned_data['message']
-        num_words = len(message.split())
-        if num_words < 4:
-            raise forms.ValidationError("Not enough words!")
-        return message
+            message = self.cleaned_data['message']
+            num_words = len(message.split())
+            if num_words < 4:
+                raise forms.ValidationError("Not enough words!")
+            return message
