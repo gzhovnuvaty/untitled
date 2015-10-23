18a98d58e87e2387221d827e6bbe2b7951e8db82
gzhovnu <g.zhovnu@gmail.com>
2015-10-23 11:43:54 +0300

    builds of potentials routes take a long time

--- books/forms.py
+++ books/forms.py
@@ -1,20 +1,25 @@
+from datetime import datetime, timedelta, date
 from django import forms
+import floppyforms as forms
+
 
 
 class ContactForm(forms.Form):
-    lat_from = forms.CharField(max_length=9)
-    lon_from = forms.CharField(max_length=9)
-    lat_to = forms.CharField(max_length=9)
-    lon_to = forms.CharField(max_length=9)
-    people = forms.IntegerField()
-    data = forms.DateTimeField()
-    time = forms.TimeField()
+    lat_from = forms.IntegerField()
+    lon_from = forms.IntegerField()
+    lat_to = forms.IntegerField()
+    lon_to = forms.IntegerField()
+    people = forms.IntegerField(initial=2, min_value=1, max_value=8)
+    user_id = forms.IntegerField(initial=17)
+    date = forms.DateField(initial=datetime.now)
+    time = forms.TimeField(initial= lambda :(datetime.today() + timedelta(hours=1)))
 
-
+    def get_current_time(self):
+        return datetime.now()
 
     def clean_message(self):
-            message = self.cleaned_data['message']
-            num_words = len(message.split())
-            if num_words < 4:
-                raise forms.ValidationError("Not enough words!")
-            return message
+        message = self.cleaned_data['message']
+        num_words = len(message.split())
+        if num_words < 4:
+            raise forms.ValidationError("Not enough words!")
+        return message
